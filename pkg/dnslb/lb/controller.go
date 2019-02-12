94fbc491fd126f4ade1789910ec8968ea7c99e9c
Martin Weindel <martin.weindel@sap.com>
2019-02-12 10:51:18 +0100

    minor cleanup

--- pkg/dnslb/lb/controller.go
+++ pkg/dnslb/lb/controller.go
@@ -2,20 +2,16 @@ package lb
 
 import (
 	api "github.com/gardener/dnslb-controller-manager/pkg/apis/loadbalancer/v1beta1"
-	"github.com/gardener/controller-manager-library/pkg/resources"
 	"github.com/gardener/external-dns-management/pkg/dns/source"
 )
-
-var _MAIN_RESOURCE = resources.NewGroupKind(api.GroupName, "DNSLoadBalancer")
 
 var OPT_BOGUS_NXDOMAIN = "bogus-nxdomain"
 
 func init() {
-	source.DNSSourceController(source.NewDNSSouceTypeForCreator("dnslb-loadbalancer", _MAIN_RESOURCE, NewDNSLBSource), nil).
+	source.DNSSourceController(source.NewDNSSouceTypeForCreator("dnslb-loadbalancer", api.LoadBalancerGroupKind, NewDNSLBSource), nil).
 		RequireLease().
 		FinalizerDomain(api.GroupName).
 		StringOption(OPT_BOGUS_NXDOMAIN, "ip address returned by DNS for unknown domain").
-		Reconciler(StateReconciler,"state").ReconcilerWatch("state", api.GroupName, api.LoadBalancerEndpointResourceKind).
+		Reconciler(StateReconciler, "state").ReconcilerWatch("state", api.GroupName, api.LoadBalancerEndpointResourceKind).
 		MustRegister("loadbalancer")
 }
-
