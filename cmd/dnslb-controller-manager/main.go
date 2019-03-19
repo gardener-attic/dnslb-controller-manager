c8cfc1a0fafebdc3211a2d8df4454690c58c1b50
Uwe Krueger <uwe.krueger@sap.com>
2019-03-19 14:58:25 +0100

    dnstarget cluster

--- cmd/dnslb-controller-manager/main.go
+++ cmd/dnslb-controller-manager/main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/gardener/dnslb-controller-manager/pkg/dnslb/endpoint/sources/ingress"
 	_ "github.com/gardener/dnslb-controller-manager/pkg/dnslb/endpoint/sources/service"
 	_ "github.com/gardener/dnslb-controller-manager/pkg/dnslb/lb"
+	"github.com/gardener/external-dns-management/pkg/dns/source"
 
 	"github.com/gardener/controller-manager-library/pkg/controllermanager"
 	"github.com/gardener/controller-manager-library/pkg/controllermanager/cluster"
@@ -12,6 +13,11 @@ import (
 )
 
 func main() {
-	mappings.Configure().ForController("dnslb-loadbalancer").Map(cluster.DEFAULT, "target").Register()
+	cluster.Configure("dnstarget", "dnstarget", "target cluster for dns entries").
+		Fallback(source.TARGET_CLUSTER).
+		Register()
+	mappings.Configure().ForController("dnslb-loadbalancer").
+		Map(cluster.DEFAULT, source.TARGET_CLUSTER).
+		Map(source.TARGET_CLUSTER, "dnstarget").Register()
 	controllermanager.Start("dnslb-controller-manager", "dns load balancer controller manager", "nothing")
 }
